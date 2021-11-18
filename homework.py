f6e71be6e07c20eb563d9d08b27cc03a41696ba0
Andrey Khan <andreykhan113@gmail.com>
2021-11-19 00:30:08 +0600

    18.11

--- homework.py
+++ homework.py
@@ -1,1 +1,17 @@
-test
+class Training:
+    def __init__(self, type_of_training, duration_of_training, distance, speed, energy_consumptions):
+        self.type_of_training = type_of_training
+        self.duration_of_training = duration_of_training
+        self.distance = distance
+        self.speed = speed
+        self.energy_consumptions = energy_consumptions
+
+class Running:
+    def __init__(self):
+
+class SportsWalking:
+    def __init__(self):
+
+class Swimming:
+    def __init__(self):
+
